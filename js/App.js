574bf20a9eae34f905fb78054c0d5b8340be2eec
hankymei <henry.mei@gatech.edu>
2013-11-06 16:41:38 -0500

    initial commit of current progress

---
+++ js/App.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    $('#main-menu-left.nav a ').on('click', function (e) {
+        $('.contentContainer').fadeTo(400, 0, function () {
+            $('.contentPane').spin();
+            $.ajax({
+                url: $(e.currentTarget).attr('data-link'),
+            }).done(function (data) {
+                $('.contentContainer').html(data);
+                $('.contentPane').spin(false);
+                $('.contentContainer').fadeTo(400, 1, function () {
+                });
+            });
+        });
+    });
+
+   
+});
