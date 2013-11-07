6dda192d7591b388d9874a42041c0d079eb77175
Matt <matt.duncan13@gmail.com>
2013-11-06 23:28:05 -0500

    Working UI commit
    Everything but bios for the staff is included. Registration is working.

--- js/App.js
+++ js/App.js
@@ -1,17 +1,15 @@
-﻿$(document).ready(function () {
-    $('#main-menu-left.nav a ').on('click', function (e) {
-        $('.contentContainer').fadeTo(400, 0, function () {
-            $('.contentPane').spin();
-            $.ajax({
-                url: $(e.currentTarget).attr('data-link'),
-            }).done(function (data) {
-                $('.contentContainer').html(data);
-                $('.contentPane').spin(false);
-                $('.contentContainer').fadeTo(400, 1, function () {
-                });
-            });
-        });
-    });
-
-   
+﻿$(document).ready(function () {
+    $('#main-menu-left.nav a ').on('click', function (e) {
+        $('.contentContainer').fadeTo(400, 0, function () {
+            $('.contentPane').spin();
+            $.ajax({
+                url: $(e.currentTarget).attr('data-link'),
+            }).done(function (data) {
+                $('.contentContainer').html(data);
+                $('.contentPane').spin(false);
+                $('.contentContainer').fadeTo(400, 1, function () {
+                });
+            });
+        });
+    });
 });
---
+++ js/Streaming.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    $('.thumbnail').on('click', function (e) {
+        console.log('click!');
+        if (!$('#streamId').hasClass('showPane'))
+            $('#streamId').addClass('showPane');
+        $('.streamContainer').fadeTo(400, 0, function () {
+            $('.streamPane').spin();
+            $.ajax({
+                url: $(e.currentTarget).attr('data-link'),
+            }).done(function (data) {
+                $('.streamContainer').html(data);
+                $('.streamPane').spin(false);
+                $('.streamContainer').fadeTo(400, 1, function () {
+                });
+            });
+        });
+    });
+});
