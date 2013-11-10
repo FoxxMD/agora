34fa7cf63f733e96034694047436e036cda685e3
hankymei <henry.mei@gatech.edu>
2013-11-10 18:03:31 -0500

    blanket commit

--- js/App.js
+++ js/App.js
@@ -1,15 +1,15 @@
-﻿$(document).ready(function () {
-    $('#main-menu-left.nav a ').on('click', function (e) {
-        $('.contentContainer').fadeTo(400, 0, function () {
-            $('.contentPane').spin();
-            $.ajax({
-                url: $(e.currentTarget).attr('data-link'),
-            }).done(function (data) {
-                $('.contentContainer').html(data);
-                $('.contentPane').spin(false);
-                $('.contentContainer').fadeTo(400, 1, function () {
-                });
-            });
-        });
-    });
+﻿$(document).ready(function () {
+    $('#main-menu-left.nav a ').on('click', function (e) {
+        $('#mainContent.contentContainer').fadeTo(400, 0, function () {
+            $('.contentPane').spin();
+            $.ajax({
+                url: $(e.currentTarget).attr('data-link'),
+            }).done(function (data) {
+                $('#mainContent.contentContainer').html(data);
+                $('.contentPane').spin(false);
+                $('#mainContent.contentContainer').fadeTo(400, 1, function () {
+                });
+            });
+        });
+    });
 });
--- js/Streaming.js
+++ js/Streaming.js
@@ -1,18 +1,18 @@
-﻿$(document).ready(function () {
-    $('.thumbnail').on('click', function (e) {
-        console.log('click!');
-        if (!$('#streamId').hasClass('showPane'))
-            $('#streamId').addClass('showPane');
-        $('.streamContainer').fadeTo(400, 0, function () {
-            $('.streamPane').spin();
-            $.ajax({
-                url: $(e.currentTarget).attr('data-link'),
-            }).done(function (data) {
-                $('.streamContainer').html(data);
-                $('.streamPane').spin(false);
-                $('.streamContainer').fadeTo(400, 1, function () {
-                });
-            });
-        });
-    });
+﻿$(document).ready(function () {
+    $('.thumbnail').on('click', function (e) {
+        console.log('click!');
+        if (!$('#streamId').hasClass('showPane'))
+            $('#streamId').addClass('showPane');
+        $('.streamContainer').fadeTo(400, 0, function () {
+            $('.streamPane').spin();
+            $.ajax({
+                url: $(e.currentTarget).attr('data-link'),
+            }).done(function (data) {
+                $('.streamContainer').html(data);
+                $('.streamPane').spin(false);
+                $('.streamContainer').fadeTo(400, 1, function () {
+                });
+            });
+        });
+    });
 });
