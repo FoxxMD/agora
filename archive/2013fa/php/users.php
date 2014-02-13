9c53d46d9d062e23975dfdf12da3b123daab5d47
Henry Mei <speshilK@Henrys-MacBook-Pro.local>
2014-02-12 23:44:06 -0500

    blanket commit after rebooting development

---
+++ archive/2013fa/php/users.php
@@ -0,0 +1,25 @@
+<?php
+
+    require_once("./lib.php");
+
+    $mode = $_POST["mode"];
+    $param1 = $_POST["param1"];
+
+    if($mode == "get")  {
+        $result = getUsers($param1);
+    } else if($mode == "set") {
+        $param2 = $_POST["param2"];
+        $param3 = $_POST["param3"];
+        $result = setUsers($param1, $param2, $param3);
+    } else if($mode == "verify") {
+        $result = verifyUser($_POST["param1"],$_POST["param2"]);
+    } else if($mode == "check") {
+        $result = ifAlreadyLogged();
+    } else if($mode == "logoff") {
+        $result = logoff();
+    } else if($mode == "delete") {
+        $result = deleteUser($_POST["param1"]);
+    }
+
+    echo $result;
+?>
