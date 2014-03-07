3bdff7f509c8b770e422b67cd690d8517aea8d23
FoxxMD <matt.duncan13@gmail.com>
2014-03-07 15:16:08 -0500

    Added calendar plugin Restructured Schedule, basic functionality complete

--- js/App.js
+++ js/App.js
@@ -17,6 +17,11 @@ app.config(['$stateProvider', '$urlRouterProvider',
                 url: '/',
                 parent: 'index'
             })
+            .state('schedule', {
+                templateUrl: 'templates/schedule.html',
+                url: '/schedule',
+                parent: 'index'
+            })
             .state('404', {
                 templateUrl: 'templates/shared/404.html',
                 url: '/404',
@@ -43,8 +48,8 @@ app.config(['RestangularProvider', '$httpProvider', function (RestangularProvide
 //Site skeleton controller
 /*
 
-    Will control site navigation and login/register actions and user notification.
-    Basically everything on the menu bar.
+ Will control site navigation and login/register actions and user notification.
+ Basically everything on the menu bar.
 
  */
 app.controller('cnc', ['$scope', '$state', '$modal', function ($scope, $state, $modal) {
@@ -72,4 +77,42 @@ app.controller('cnc', ['$scope', '$state', '$modal', function ($scope, $state, $
         }
     };
 
+}]);
+
+app.directive('scheduleDirective', ['$compile', function ($compile) {
+    var directive = {
+        restrict: 'AE',
+        template: '<button class="btn btn-info pull-right" ng-click="changeDay(\'2014-03-08\')">Day 1</button><button class="btn btn-info pull-right" ng-click="changeDay(\'2014-03-09\')">Day 2</button><div id="calendar" style="float:left;"></div>',
+        link: function (scope, element, attrs) {
+
+               var calendar = $(element).find('#calendar').calendar({
+                    events_source: [
+                        {
+                            'id': 1,
+                            'title': 'Test Event',
+                            'start': 1394301600000,
+                            'end': 1394308800000,
+                            'class':'event-inverse'
+                        },
+                        {
+                            'id': 2,
+                            'title': 'Test Event 2',
+                            'start': 1394305200000,
+                            'end': 1394312400000,
+                            'class':'event-inverse'
+                        }
+                    ],
+                    tmpl_path:'js/calendar/tmpls/',
+                    view:'day',
+                    day: '2014-03-08'
+                });
+
+            scope.changeDay = function(theDay){
+                calendar.setOptions({day: theDay});
+                calendar.view();
+            };
+
+        }
+    };
+    return directive;
 }]);
