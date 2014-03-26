dd35d77b4c091fd2dd86ed3324b50274901f5cfe
FoxxMD <matt.duncan13@gmail.com>
2014-03-26 11:59:17 -0400

    Added automated email on registration, closes #22

--- php/register.php
+++ php/register.php
@@ -24,6 +24,23 @@
         if($statement -> execute())
         {
             $response -> success = true;
+
+            $subject = "Welcome to GT Gamefest!";
+            $headers   = array();
+            $headers[] = "MIME-Version: 1.0";
+            $headers[] = "Content-type: text/html; charset=iso-8859-1";
+            $headers[] = "From: GT Gamefest <noreply@gtgamefest.com>";
+            $headers[] = "Subject: {$subject}";
+            $headers[] = "X-Mailer: PHP/".phpversion();
+            $headers.="Return-Path:<noreply@gtgamefest.com>";
+
+            $body = "Hello New User,\r\n\r\n You have just registered for Georgia Tech's GameFest taking place April 19th-20th.\r\n\r\n To complete your registration please login at http://beta.gtgamefest.com and pay your registration fee, this will allow you to particiapte in any tournaments taking place during the event.\r\n\r\n See You There,\r\n-GT Gamefest Staff";
+
+            if(!mail($user -> email, $subject, $body, implode("\r\n",$headers), "-f noreply@gtgamefest.com"))
+            {
+                error_log("Sending email was unsuccessful for ".$user -> email);
+            }
+
             $response -> authtoken = $authToken;
         }
         else{
