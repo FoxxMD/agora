06760cdb51e1bdb212d46b19ef1df03046afa80a
FoxxMD <matt.duncan13@gmail.com>
2014-08-25 10:51:16 -0400

    working mailgun wrapper implemeneted #122 #129

--- src/main/scala/com/esports/gtplatform/Utilities/Mailer.scala
+++ src/main/scala/com/esports/gtplatform/Utilities/Mailer.scala
@@ -1,44 +1,52 @@
 package com.esports.gtplatform.Utilities
 
-import com.esports.gtplatform.Utilities.MailgunMine._
-import org.slf4j.LoggerFactory
-import scala.concurrent.ExecutionContext.Implicits.global
-import skinny.mailer._
-/*import com.roundeights.mailgun.{Email, MailSender, Mailgun}*/
 
-import scala.concurrent.Future
+import java.util.concurrent.TimeoutException
+
+import com.roundeights.mailgun.{Email, MailSender, Mailgun}
+import org.slf4j.LoggerFactory
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
 
 /**
  * Created by Matthew on 8/20/2014.
  */
 class Mailer(eventName: String = "Gamefest") {
   val logger = LoggerFactory.getLogger(getClass)
-  class MyMailer extends SkinnyMailer
   val sender: MailSender =  new Mailgun("sandboxebc2e9178fa5401b9169d5c545a89379.mailgun.org", "key-77e9200b3bc681ca40e0c58267b13941")
+  val defaultFrom = "postmaster@sandboxebc2e9178fa5401b9169d5c545a89379.mailgun.org"
 
-/*  def sendConfirm(toAddress: String, handle: String, token: String) = {
-    val mail = new MyMailer
+  private def sendMail(mail: Email) = {
+    val response: Future[MailSender.Response] = sender.send(mail)
 
-    mail.
-    to(toAddress).
-    subject("Confirm your registration for " + eventName).
-    body(
-      s"""Hello $handle!
-        |
-        | Please complete your registration and confirm this email address was used by you to register for $eventName
-        | by following this link: http://gtgamefest.com/confirmRegistration?token=$token
-        |
-        | Thanks!
-        |
-        | -$eventName Staff
-      """.stripMargin).deliver()
-  }*/
+    try {
+      Await.result(
+        response.map(data => {
+          println( data.id )
+          println( data.message )
+          logger.info("Email successfully sent.")
+        }),
+        5.seconds
+      )
+    }
+    catch {
+      case (t: TimeoutException) =>
+        logger.info("Email took longer than 5 seconds to send. Network or mailgun problem?")
+    }
+    finally {
+      sender.close
+    }
+  }
+
 def sendConfirm(toAddress: String, handle: String, token: String) = {
 
-  val response: Future[MailSender.Response] = sender.send(
-  to = Email.Addr(handle, toAddress),
-  from = Email.Addr(eventName + " Staff", "postmaster@sandboxebc2e9178fa5401b9169d5c545a89379.mailgun.org"),
-  subject = "a test email",
+  val confirmEmail =
+    Email(
+  to = Email.Addr(toAddress, handle),
+  from = Email.Addr(defaultFrom, eventName + " Staff"),
+  subject = s"Confirm Your Account for $eventName",
   body = Email.text(s"""Hello $handle!
         |
         | Please complete your registration and confirm this email address was used by you to register for $eventName
@@ -49,21 +57,18 @@ def sendConfirm(toAddress: String, handle: String, token: String) = {
         | -$eventName Staff
       """.stripMargin)
   )
-  logger.info("Mail was sent? " + response.isCompleted)
-  response.map(r => {
-    logger.info(r.id)
-    logger.info(r.message)
-  })
+  logger.info("Attempting to send confirmation email to " + toAddress)
+  sendMail(confirmEmail)
 }
 
   def sendAlreadyRegistered(toAddress: String) = {
-    val mail = new MyMailer
 
-    mail.
-      to(toAddress).
-      subject("Confirm your registration for " + eventName).
-      body(
-        s"""Hello,
+    val mail =
+      Email(
+        to = Email.Addr(toAddress),
+        from = Email.Addr(defaultFrom, eventName + " Staff"),
+        subject = s"Registered Account for $eventName",
+        body = Email.text(s"""Hello,
         |
         | Someone tried to use your email address to register for $eventName, but this address is already associated
         | to an account! If you have forgotten your password please visit http://gtgamefest.com/resetPassword to start
@@ -72,7 +77,11 @@ def sendConfirm(toAddress: String, handle: String, token: String) = {
         | Thanks!
         |
         | -$eventName Staff
-      """.stripMargin).deliver()
+      """.stripMargin)
+      )
+    logger.info("Attempting to send already registered email to " + toAddress)
+    sendMail(mail)
+
   }
 
 }
