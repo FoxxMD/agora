a025a9604024ff4627d4e31723dbcd12bd50589c
FoxxMD <matt.duncan13@gmail.com>
2014-08-25 16:54:15 -0400

    Added some team funtionality

--- src/main/scala/com/esports/gtplatform/controllers/TeamController.scala
+++ src/main/scala/com/esports/gtplatform/controllers/TeamController.scala
@@ -4,7 +4,7 @@ import com.escalatesoft.subcut.inject.BindingModule
 import com.esports.gtplatform.business.GenericMRepo
 import com.googlecode.mapperdao.jdbc.Transaction
 import models._
-import org.scalatra.{BadRequest, Ok}
+import org.scalatra.{Forbidden, BadRequest, Ok}
 
 /**
  * Created by Matthew on 7/29/2014.
@@ -20,10 +20,10 @@ class TeamController(implicit val bindingModule: BindingModule) extends APIContr
     auth()
     val newteam = parsedBody.extract[Team]
     val teamUserRepo = inject[GenericMRepo[TeamUser]]
-    var userFT:User = user
+    var userFT: User = user
 
     params.get("captainId") match {
-      case Some(a:String) =>
+      case Some(a: String) =>
         if (user.role == "admin") {
           val personRepo = inject[GenericMRepo[User]]
           val thisUser = personRepo.get(a.toInt)
@@ -43,11 +43,56 @@ class TeamController(implicit val bindingModule: BindingModule) extends APIContr
     }
     Ok(tid)
   }
-  get("/:id"){
+  get("/:id") {
     val teamId = params.getOrElse("id", halt(401, "Id parameter is missing"))
 
     teamRepo.get(teamId.toInt) match {
       case Some(t: Team) => Ok(t)
+      case None => BadRequest("No team exists with the Id " + teamId)
+    }
+  }
+  post("/:id/addMember") {
+    val teamId = params.getOrElse("id", halt(401, "Team Id parameter is missing"))
+    val addingUser = parsedBody.\("userId").extractOrElse(halt(401, "User Id parameter is missing"))[String]
+    auth()
+    teamRepo.get(teamId.toInt) match {
+      case Some(t: Team) =>
+        if (user.role == "admin" || t.getCaptain == user) {
+          val userRepo = inject[GenericMRepo[User]]
+          userRepo.get(addingUser.toInt) match {
+            case Some(u: User) =>
+              val newTP = t.teamPlayers.+:(TeamUser(t, u, isCaptain = false)) //TODO use lensing
+              val newt = t.copy(teamPlayers = newTP)
+              teamRepo.update(t, newt)
+            case None => BadRequest("No user with that Id exists.")
+          }
+        }
+        else {
+          Forbidden("You don't have rights to modify this team.")
+        }
+      case None => BadRequest("No team exists with the Id " + teamId)
+    }
+  }
+  post("/:id/removeMember") {
+    val teamId = params.getOrElse("id", halt(401, "Team Id parameter is missing"))
+    val addingUser = parsedBody.\("userId").extractOrElse(halt(401, "User Id parameter is missing"))[Int]
+    auth()
+    teamRepo.get(teamId.toInt) match {
+      case Some(t: Team) =>
+        if (user.role == "admin" || t.getCaptain == user) {
+          if(t.teamPlayers.exists(x => x.user.id == addingUser))
+          {
+            val newTP = t.teamPlayers.filter(x => x.user.id != addingUser)
+            val newt = t.copy(teamPlayers = newTP)
+            teamRepo.update(t, newt)
+          }
+          else{
+            BadRequest("User with that Id is not on this team")
+          }
+        }
+        else {
+          Forbidden("You don't have rights to modify this team.")
+        }
       case None => BadRequest("No team exists with the Id " + teamId)
     }
   }
