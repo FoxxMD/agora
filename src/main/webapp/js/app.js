e2985aed73c169df9c5588e1d41c3b5f776a281f
FoxxMD <matt.duncan13@gmail.com>
2014-10-14 18:42:51 -0400

    Removed angular-resource (not using)

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,6 +1,6 @@
 // Declare app level module which depends on filters, and services
 // @ngInject
-angular.module('gtfest', ['ngResource', 'ui.bootstrap', 'restangular', 'ui.router', 'ngStorage',
+angular.module('gtfest', ['ui.bootstrap', 'restangular', 'ui.router', 'ngStorage',
         'ui.bootstrap.showErrors', 'ngAnimate', 'ui.validate', 'angular-loading-bar', 'ngSanitize','angular-ladda',
         'xeditable','angularPayments', 'toggle-switch', 'ui.calendar','infinite-scroll','wu.masonry','ngTagsInput','ui.tree',
         'angulartics', 'angulartics.google.analytics', 'toaster'],
